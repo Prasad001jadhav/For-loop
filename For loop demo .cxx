d8753af53913d72299e622ea9352edc1a8e122e4
Forkwebtech <144381314+Forkwebtech@users.noreply.github.com>
2023-09-16 09:36:37 +0530

    Add files via upload

---
+++ For loop demo .cxx
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+   int i = 0;
+   
+   for (i = 2; i<=5; i++)
+   {
+   	printf("\n %d",i);
+   	printf("\n prasad");
+   	printf("\n Raj");
+   	printf("\n vivak");
+   	printf("\n sahil");
+    }
+    
+    printf("\n\n thanks");
+    
+    getch();
+    return 0;
+}	
